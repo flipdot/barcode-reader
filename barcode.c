c79685fcd1dedda11d63e73fb717f74c88d9d57a
Jonas Platte <mail@jonasplatte.de>
2015-07-09 20:43:40 +0200

    Removed letter handling, using stderr for error messages

--- barcode.c
+++ barcode.c
@@ -16,52 +16,57 @@ typedef struct input_event input_event;
 // This script prints a bar code followed by a \n.
 // just read the stdout.
 
+void usage(const char* prog)
+{
+	fprintf(stderr, "usage: %s", prog);
+	exit(1);
+}
+
 char get_char_from_scancode(byte code)
 {
-	// TODO: Add support for more scan codes and stuff
 	// See: http://www.cs.fsu.edu/~baker/devices/lxr/http/source/linux/include/linux/input.h?v=2.6.11.8
 	if(KEY_1 <= code && code <= KEY_9)
 		return '1' + (code - KEY_1);
 	if(code == KEY_0)
 		return '0';
-	printf("missing: %d\n", code);
+
+	fprintf(stderr, "missing: %d\n", code);
 	return 'E';
 }
 
 int main(int argc, char** argv)
 {
-	FILE *input = fopen("/dev/input/by-id/usb-GASIA_PS2toUSB_Adapter-event-kbd", "rb");
-	if(input == nullptr)
+	if (argc != 2)
+		usage(argv[0]);
+
+	// argv[1] is usually "/dev/input/by-id/usb-GASIA_PS2toUSB_Adapter-event-kbd"
+	FILE* input = fopen(argv[1], "rb");
+	if (input == nullptr)
+	{
+		fprintf(stderr, "Couldn't open %s", argv[0]);
 		return 1;
+	}
 
-	bool isUpperCase = false;
-
-	while(!feof(input))
+	while (!feof(input))
 	{
 		input_event ev;
 		char current_char = 0;
 		size_t read_bytes = fread(&ev, 1, sizeof(input_event), input);
-		if(read_bytes != sizeof(input_event))
+		if (read_bytes != sizeof(input_event))
 		{
-			fputs("lolzuklein", stderr);
+			fprintf(stderr, "fread returned early");
 			fclose(input);
 			input = nullptr;
-			return 2;
+			return 1;
 		}
-		switch(ev.type)
+
+		switch (ev.type)
 		{
-			case EV_SYN: continue; // skip sync codes
+			case EV_SYN: break; // skip sync codes
 			case EV_KEY:
-				if(ev.code == KEY_LEFTSHIFT)
+				if (ev.code == KEY_TAB)
 				{
-					// toggle shift keys
-					// may be used in later versions
-					isUpperCase = (ev.value == KEY_PRESS);
-					continue;
-				}
-				if(ev.code == KEY_TAB)
-				{
-					if(ev.value == KEY_RELEASE)
+					if (ev.value == KEY_RELEASE)
 					{
 						putchar('\n');
 						fflush(stdout);
@@ -69,7 +74,8 @@ int main(int argc, char** argv)
 					continue;
 				}
 
-				if(ev.value == KEY_RELEASE) continue;
+				if (ev.value == KEY_RELEASE)
+					continue;
 
 				current_char = get_char_from_scancode(ev.code);
 				putchar(current_char);
@@ -78,10 +84,11 @@ int main(int argc, char** argv)
 				//printf("EV_MSC\tcode: %x value: %x", ev.code, ev.value);
 				break;
 			default:
-				printf("Unmatched type: %x", ev.type);
+				fprintf(stderr, "Unmatched type: %x", ev.type);
 				break;
 		}
 	}
+
 	fclose(input);
 	input = nullptr;
 	return 0;
