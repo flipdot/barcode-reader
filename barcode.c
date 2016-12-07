c41119924bdfc1a8b050ca28e971b03c2e79cbf1
Claus Strasburger <claus@strasburger.de>
2016-12-07 01:21:38 +0100

    support characters in barcodes, ETSCHUNK

--- barcode.c
+++ barcode.c
@@ -13,6 +13,14 @@ typedef unsigned char byte;
 typedef unsigned char bool;
 typedef struct input_event input_event;
 
+typedef struct {
+	byte code;
+	char c;
+} key_entry;
+
+key_entry alphabet[] = {{KEY_A, 'A'}, {KEY_B, 'B'}, {KEY_C, 'C'}, {KEY_D, 'D'}, {KEY_E, 'E'}, {KEY_F, 'F'}, {KEY_G, 'G'}, {KEY_H, 'H'}, {KEY_I, 'I'}, {KEY_J, 'J'}, {KEY_K, 'K'}, {KEY_L, 'L'}, {KEY_M, 'M'}, {KEY_N, 'N'}, {KEY_O, 'O'}, {KEY_P, 'P'}, {KEY_Q, 'Q'}, {KEY_R, 'R'}, {KEY_S, 'S'}, {KEY_T, 'T'}, {KEY_U, 'U'}, {KEY_V, 'V'}, {KEY_W, 'W'}, {KEY_X, 'X'}, {KEY_Y, 'Y'}, {KEY_Z, 'Z'},
+{KEY_SLASH, '_'}};
+
 // This script prints a bar code followed by a \n.
 // just read the stdout.
 
@@ -29,9 +37,13 @@ char get_char_from_scancode(byte code)
 		return '1' + (code - KEY_1);
 	if(code == KEY_0)
 		return '0';
-
+	
+	for (size_t i=0; i<sizeof(alphabet)/sizeof(key_entry); i++) {
+		if (code == alphabet[i].code)
+			return alphabet[i].c;
+	}
 	fprintf(stderr, "missing: %d\n", code);
-	return 'E';
+	return '\0';
 }
 
 int main(int argc, char** argv)
@@ -46,7 +58,8 @@ int main(int argc, char** argv)
 		fprintf(stderr, "Couldn't open %s", argv[0]);
 		return 1;
 	}
-
+	
+	int char_num = 0;
 	while (!feof(input))
 	{
 		input_event ev;
@@ -68,6 +81,7 @@ int main(int argc, char** argv)
 				{
 					if (ev.value == KEY_RELEASE)
 					{
+						char_num = 0;
 						putchar('\n');
 						fflush(stdout);
 					}
@@ -77,6 +91,9 @@ int main(int argc, char** argv)
 				if (ev.value == KEY_RELEASE)
 					continue;
 
+				if (char_num++ == 0) {
+					putchar('E');
+				}
 				current_char = get_char_from_scancode(ev.code);
 				putchar(current_char);
 				break;
