e8a87e275c74eda5811b86ed5df570143e7016ce
Claus Strasburger <claus@strasburger.de>
2017-03-01 00:27:05 +0100

    swap y and z... because I don't want to figure out the driver settings

--- barcode.c
+++ barcode.c
@@ -18,7 +18,7 @@ typedef struct {
 	char c;
 } key_entry;
 
-key_entry alphabet[] = {{KEY_A, 'A'}, {KEY_B, 'B'}, {KEY_C, 'C'}, {KEY_D, 'D'}, {KEY_E, 'E'}, {KEY_F, 'F'}, {KEY_G, 'G'}, {KEY_H, 'H'}, {KEY_I, 'I'}, {KEY_J, 'J'}, {KEY_K, 'K'}, {KEY_L, 'L'}, {KEY_M, 'M'}, {KEY_N, 'N'}, {KEY_O, 'O'}, {KEY_P, 'P'}, {KEY_Q, 'Q'}, {KEY_R, 'R'}, {KEY_S, 'S'}, {KEY_T, 'T'}, {KEY_U, 'U'}, {KEY_V, 'V'}, {KEY_W, 'W'}, {KEY_X, 'X'}, {KEY_Y, 'Y'}, {KEY_Z, 'Z'},
+key_entry alphabet[] = {{KEY_A, 'A'}, {KEY_B, 'B'}, {KEY_C, 'C'}, {KEY_D, 'D'}, {KEY_E, 'E'}, {KEY_F, 'F'}, {KEY_G, 'G'}, {KEY_H, 'H'}, {KEY_I, 'I'}, {KEY_J, 'J'}, {KEY_K, 'K'}, {KEY_L, 'L'}, {KEY_M, 'M'}, {KEY_N, 'N'}, {KEY_O, 'O'}, {KEY_P, 'P'}, {KEY_Q, 'Q'}, {KEY_R, 'R'}, {KEY_S, 'S'}, {KEY_T, 'T'}, {KEY_U, 'U'}, {KEY_V, 'V'}, {KEY_W, 'W'}, {KEY_X, 'X'}, {KEY_Y, 'Z'}, {KEY_Z, 'Y'},
 {KEY_SLASH, '_'}};
 
 // This script prints a bar code followed by a \n.
@@ -95,8 +95,9 @@ int main(int argc, char** argv)
 					putchar('E');
 				}
 				current_char = get_char_from_scancode(ev.code);
-				if current_char:
+				if (current_char) {
 					putchar(current_char);
+				}
 				break;
 			case EV_MSC:
 				//printf("EV_MSC\tcode: %x value: %x", ev.code, ev.value);
